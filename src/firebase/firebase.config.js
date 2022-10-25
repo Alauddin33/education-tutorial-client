63433720022f6ee4e1cba1a058c44283832ecae6
Alauddin33 <shamem33@gmail.com>
2022-10-25 11:52:33 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCW76OtuhCzdYSrKsJJa05IViuiSAKVZik",
+    authDomain: "education-c0466.firebaseapp.com",
+    projectId: "education-c0466",
+    storageBucket: "education-c0466.appspot.com",
+    messagingSenderId: "58030354546",
+    appId: "1:58030354546:web:641ec13dd1840517af12eb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
